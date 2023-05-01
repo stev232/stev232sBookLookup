f780c363218c83416d7d7357b052d62ccc9f09df
stev232 <stev232@yahoo.com>
2023-05-01 18:24:24 -0500

    adjusted the mutations and queries on the client side to work with this project.

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,49 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation addBook($bookId: ID!) {
+    addBook(bookId: $bookId) {
+      bookId
+      authors
+      description
+      title
+      image
+      link
+      User {
+        savedBooks
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      savedBooks
+    }
+  }
+`;
