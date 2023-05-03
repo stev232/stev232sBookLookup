dae6552501e6e6a4484d2f9d42b9bdc619d6cc37
stev232 <stev232@yahoo.com>
2023-05-02 21:50:10 -0500

    took connection code from the class material.

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
 
-var uri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks'
 
-mongoose.connect(uri); 
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/tech-friends',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+); 
 
 module.exports = mongoose.connection;
