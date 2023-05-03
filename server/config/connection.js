594b21faec3f6e4dc49801ea38ab017f1f87d025
stev232 <stev232@yahoo.com>
2023-05-02 21:42:01 -0500

    updated readme and I have one more idea for troubleshooting heroku.

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+var uri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks'
+
+mongoose.connect(uri); 
 
 module.exports = mongoose.connection;
